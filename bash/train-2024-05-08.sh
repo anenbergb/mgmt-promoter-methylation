5f079b99923a90ebbf2f3dee477f9016a040fcb2
Bryan Anenberg <anenbergb@gmail.com>
2025-05-14 14:09:11 -0700

    notebooks and other updates

--- bash/train-2024-05-08.sh
+++ bash/train-2024-05-08.sh
@@ -9,7 +9,7 @@ python mgmt/tasks/train.py \
 OUTPUT_DIR $EXPR_DIR \
 SEED_EVERYTHING 1 \
 TRAINER.max_epochs 100 \
-DATA.BATCH_SIZE 12 \
-TRAINER.limit_train_batches 10 \
-TRAINER.limit_val_batches 3
+DATA.BATCH_SIZE 12
+# TRAINER.limit_train_batches 10 \
+# TRAINER.limit_val_batches 3
 # CHECKPOINT.PATH last
